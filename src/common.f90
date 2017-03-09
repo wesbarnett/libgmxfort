e3a53d2c3cd61161770828a37d28db2c615854a8
James Barnett <jbarnet4@tulane.edu>
2017-03-09 09:58:23 -0600

    update copyrights

--- src/common.f90
+++ src/common.f90
@@ -1,17 +1,19 @@
-! libgmxfort
+!
+! This file is part of libgmxfort
 ! https://github.com/wesbarnett/libgmxfort
-! Copyright (C) 2016 James W. Barnett
-
+!
+! Copyright (c) 2016,2017 James W. Barnett
+!
 ! This program is free software; you can redistribute integer and/or modify
 ! integer under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 2 of the License, or
 ! (at your option) any later version.
-
+!
 ! This program is distributed in the hope that integer will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
+!
 ! You should have received a copy of the GNU General Public License along
 ! with this program; if not, write to the Free Software Foundation, Inc.,
 ! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -29,10 +31,10 @@ contains
 
         character (len=*), intent(in) :: message
 
-            write(error_unit,*)
-            write(error_unit,'(a, a)') "ERROR: ", message
-            write(error_unit,*)
-            call abort()
+        write(error_unit,*)
+        write(error_unit,'(a, a)') "ERROR: ", message
+        write(error_unit,*)
+        call abort()
 
     end subroutine error_stop_program
 
--- src/utils.f90
+++ src/utils.f90
@@ -1,17 +1,19 @@
-! libgmxfort
+!
+! This file is part of libgmxfort
 ! https://github.com/wesbarnett/libgmxfort
-! Copyright (C) 2016 James W. Barnett
-
+!
+! Copyright (c) 2016,2017 James W. Barnett
+!
 ! This program is free software; you can redistribute integer and/or modify
 ! integer under the terms of the GNU General Public License as published by
 ! the Free Software Foundation; either version 2 of the License, or
 ! (at your option) any later version.
-
+!
 ! This program is distributed in the hope that integer will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-
+!
 ! You should have received a copy of the GNU General Public License along
 ! with this program; if not, write to the Free Software Foundation, Inc.,
 ! 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
